7e19f1c2ee3cf7ac1a0d6cab90a8b48faca689a5
alvaroblancoperez98 <alvaro.blanco002@gmail.com>
2023-02-13 18:41:22 +0100

    mejorado

--- models/peliculas.js
+++ models/peliculas.js
@@ -4,7 +4,16 @@ const Schema = mongoose.Schema;
 const peliculasSchema = new Schema({
     nombre: String,
     genero: String,
-    duracion: String
+    duracion: String,
+    director: String,
+    fechareserva: {
+     type: Date,
+     default:Date.now
+    },
+    fechadevolucion: {
+        type:Date,
+        required:true
+    }
 })
 
 //Creamos el modelo
